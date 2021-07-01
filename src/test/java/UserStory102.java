df557c1fb9e94e1157bd63049c313e36bba95746
KunduzKoyu <kunduzazamat@gmail.com>
2021-06-30 20:19:41 -0500

    User Story 102 has finished

--- src/test/java/UserStory102.java
+++ src/test/java/UserStory102.java
@@ -1,2 +1,6 @@
 public class UserStory102 {
+    //lots of code written here
+    // ready to push this branch to remote
+    // and open pull request so I can start collaborating
+    // so my team member can review and approve or request for more changes
 }
