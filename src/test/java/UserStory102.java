8cb1e47eef9274c03ffafded41e5aed01c7257d2
Kunduz <82919119+KunduzKoyu@users.noreply.github.com>
2021-06-30 20:27:52 -0500

    Us102 has been completed (#2)
    Combined all 3 commit by squashing it

---
+++ src/test/java/UserStory102.java
@@ -0,0 +1,7 @@
+public class UserStory102 {
+    //lots of code written here
+    // ready to push this branch to remote
+    // and open pull request so I can start collaborating
+    // so my team member can review and approve or request for more changes
+    // team member asked for change
+}
